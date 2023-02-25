329b3bf1592d33aadaf30256fe6432f8e6a568c5
Warren <hello@warren.codes>
2023-02-25 13:17:17 -0700

    Add sum function; add number count edge-case

--- sum.js
+++ sum.js
@@ -1,1 +1,41 @@
-// // Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though. If any argument is not a number, output an error message. We need at least two arguments.
+// Write a node program that takes in an unlimited number of command
+// line arguments, goes through each and prints out the sum of them.
+// If any argument is not a whole number, skip it. Do support negative
+// numbers though. If any argument is not a number, output an error
+// message. We need at least two arguments.
+
+// 1. Identify INPUT(S)
+// 2. Identify OUTPUT / (identify edge-cases)
+// 3. PROCESSING ---> Break it into smaller pieces...
+            //   ---> Break THOSE pieces into smaller pieces...
+
+/**
+ * sum of command-line numbers provided by user
+ * 
+ * @param {Array<number>} numbers Array of numbers from CLI
+ * @return {number} Sum of all provided numbers
+ */
+function sum(numbers) {
+    let sum = 0;
+
+    // make sure there are at least 2 numbers
+    if(numbers.length < 2) {
+        return console.log('NEEDS TO BE AT LEAST TWO NUMBERS.');
+    }
+
+    // command-line numbers...
+
+    // loop through numbers
+
+        // make sure it is a whole number (or output error)
+
+        // add numbers to the sum total
+
+    return sum;
+}
+
+console.log(
+    sum([1]), // Show error
+    sum([1, 2, 3]) // 6
+);
+
